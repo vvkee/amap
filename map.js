bfeae2d082ae7fc42280c921dd5996d8f5328385
韦宗圻 <weizongqi@56xdd.com>
2015-08-19 17:38:05 +0800

    update

--- map.js
+++ map.js
@@ -35,35 +35,73 @@ Map.prototype.getMap = function(){
 
 /**
  * [设置marker以及图标]
- * @param  {point,url,size} option [description]
+ * @param  {point,img,size,offset} option [description]
  * @return marker [返回一个marker对象]
  */
-Map.prototype.createMarker = function(option){
-	if(!option.point){
+Map.prototype.createMarker = function(_option){
+	if(!_option.point){
 		console.log('创建标注时，未添加point！');
 		return
 	};
-	if(typeof option.size === 'undefined' || !option.size){
-		option.size = {};
+	var option = {
+		point: _option.point,
+		img: _option.img || __inline('/order/static/image/trace/marker-crab.png'),
+		offset: _option.offset || {},
+		size: _option.size || {}
 	}
-	if(typeof option.url === 'undefined' || !option.url){
-		var img = __inline('/order/static/image/trace/marker-crab.png');
-	}
-	var marker_size = {
+	var offset = {
+		left: option.offset.left || 0,
+		top: option.offset.top || 0
+	};
+	var size = {
 		width: option.size.width || 35,
 		height: option.size.height || 29
-	};
-	var point = option.point;
-	var marker_url = option.url || '';
+	}
+	var point = new AMap.LngLat(option.point.lng, option.point.lat);
 
 	var marker = new AMap.Marker({
 		icon: new AMap.Icon({    
 			//图标大小
-			size: new AMap.Size(marker_size.width, marker_size.height),
+			size: new AMap.Size(size.width, size.height),
 			//大图地址
-			image: img
+			image: option.img
 		}),
-		position: new AMap.LngLat(point.lng, point.lat)
+		position: point,
+		offset: new AMap.Pixel(offset.left, offset.top) //相对于基点的偏移位置
+	});
+
+	marker.setMap(this.map);
+	return marker;
+}
+
+
+/**
+ * [自定义marker]
+ * @param  {point,html,offset} option [description]
+ * @return marker [返回一个marker对象]
+ */
+Map.prototype.createBespokeMarker = function(_option){
+	if(!_option.point){
+		console.log('创建信息窗口时，未添加point！');
+		return
+	};
+	var option = {
+		point: _option.point,
+		html: _option.html || '',
+		offset: _option.offset || {}
+	}
+	var offset = {
+		left: option.offset.left || 0,
+		top: option.offset.top || 0
+	};
+	var point = new AMap.LngLat(option.point.lng, option.point.lat);
+
+	var marker = new AMap.Marker({
+		map:this.map,
+		position: point, //基点位置
+		offset: new AMap.Pixel(offset.left, offset.top), //相对于基点的偏移位置
+		draggable: false,  //是否可拖动
+		content: option.html   //自定义点标记覆盖物内容
 	});
 
 	marker.setMap(this.map);
